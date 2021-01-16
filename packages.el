0d3357f86c5b6e7cb90f86642ca36e597453edce
wangshuai <549360972@qq.com>
2021-01-16 16:26:27 +0800

    add exec-path-from-shell

--- packages.el
+++ packages.el
@@ -48,3 +48,6 @@
 ;(unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;(unpin! t)
+
+;; this package ensure environment variables inside Emacs look the same as in the user's shell.
+;;(package! exec-path-from-shell)
